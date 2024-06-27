7540f8d7c24212a66d289e0024741ffffa81ab5a
coniargento <argentoconstanza@gmail.com>
2024-06-27 20:23:08 -0300

    correccionvercel

--- src/app/details/page.js
+++ src/app/details/page.js
@@ -1,53 +1,9 @@
-'use client';
-import React from 'react';
-import { useRouter } from 'next/navigation';
-import { useSearchParams } from 'next/navigation';
-import { useState, useEffect } from 'react';
+import React from 'react'
+import PageDetails from '@/app/components/PageDetails'
 
-const Details = () => {
-  const router = useRouter();
-  const name = useSearchParams().get('name');
-  const vicinity = useSearchParams().get('vicinity');
-
-  const generateStarsHTML = (rating) => {
-    const fullStarWidth = (rating / 5) * 100; // Regla de 3 simple para el ancho de las estrellas llenas
-    return (
-      <div className="rating-container">
-        <div className="stars-container">
-          <div className="stars-background"></div>
-          <div className="stars-foreground" style={{ width: `${fullStarWidth}%` }}></div>
-        </div>
-        <span className="rating-value">{rating}</span>
-      </div>
-    );
-  };
-
-  const [selectedPlace, setSelectedPlace] = useState(null);
-
-  useEffect(() => {
-    const place = localStorage.getItem('selectedPlace');
-    if (place) {
-      setSelectedPlace(JSON.parse(place));
-    }
-  }, []);
-
-  if (!selectedPlace) {
-    return <div>No place selected</div>;
-  }
-
+export default function page() {
   return (
-    <div className="paginainterna">
-      <div className="tituloboton">
-        <button className="botonatras" onClick={() => router.back()}>x</button>
-        <h1>{name}</h1>
-      </div>
-      <p className="datos">{vicinity}</p>
-      <div className="datos">{generateStarsHTML(selectedPlace.rating)}</div>
-      {selectedPlace.photo && (
-        <img className="imgpage" src={selectedPlace.photo} alt={`Photo of ${name}`} />
-      )}
-    </div>
-  );
-};
+    <div><PageDetails/></div>
+  )
+}
 
-export default Details;
